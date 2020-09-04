d1ff55f0560b73a5553901a4ab791bdec3463207
Misskanaissa <misskanaissa@gmail.com>
2020-09-04 17:19:22 -0500

    Creating Schemas

---
+++ app_api/routes/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const ctrlTeachers = require('../controllers/teachers');
+const crtlCalendar = require('../controllers/calendar');
+const ctrlReviews =require('../controllers/reviews');
+
+//teachers
+router
+    .route('/teachers')
+    .get(ctrlTeachers.teacherInfo)
+    .post(ctrlTeachers.teachersCreate);
+
+router
+    .route('/teachers/:teacherid')
+    .get(ctrlTeachers.teachersReadOne)
+    .put(ctrlTeachers.teachersUpdateOne)
+    .delete(ctrlTeachers.teachersDeleteOne);
+
+//calendar
+router
+    .route('/teachers/:teacherid/calendar')
+    .get(crtlCalendar.calendarCreate);
+
+router
+    .route('/teachers/:teacherid:/calendar/:calendarid')
+    .get(crtlCalendar.calendarReadOne)
+    .put(crtlCalendar.calendarUpdateOne)
+    .delete(crtlCalendar.calendarDeleteOne);
+
+//reviews
+router
+    .route('/teachers/:teacherid/reviews')
+    .post(ctrlReviews.reviewsCreate);
+
+router
+    .route('/teachers/:teacherid/reviews/:reviewid')
+    .get(ctrlReviews.reviewsReadOne)
+    .put(ctrlReviews.reviewsUpdateOne)
+    .delete(ctrlReviews.reviewsDeleteOne);
+
+module.exports = router;
+
+
+
--- app_server/models/db.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost/BookATeacher';
